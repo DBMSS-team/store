4574bb5a88d52931e567dc703c4296e8493d3e41
I355102 <sahil.kumar@sap.com>
2020-09-04 20:45:24 +0530

    store_items taken directly from products schema

--- db/models/store.model.js
+++ db/models/store.model.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
+const addressSchema = require('../../../users/db/models/user.model').addressSchema;
+const storeItemsSchema = require('../../../products/db/models/product.model').productSchema;
 
 const Schema = mongoose.Schema;
-
-const storeItemsSchema = new Schema({
-	product_id: { type: String, required: true },
-	quantity: { type: Number, required: true },
-	price: { type: mongoose.Decimal128, required: true },
-});
 
 const storeSchema = new Schema(
 	{
 		name: { type: String, required: true },
-		address_id: { type: String, required: true },
+		address: { type: addressSchema, required: true },
 		category_id: { type: String, required: true },
 		location_id: { type: String, required: true },
 		store_items: [storeItemsSchema],
