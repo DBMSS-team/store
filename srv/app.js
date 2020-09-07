f1f2f15b284c2dc17cf89c68ca00482222383d06
deepakbharadwaj <32411411+deepakbharadwaj@users.noreply.github.com>
2020-09-07 23:34:23 +0530

    app.js now uses commons

--- srv/app.js
+++ srv/app.js
@@ -1,35 +1,71 @@
+// Global variables declaration for commons submodule
+global.__commons = __dirname + "/commons/index";
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const { authorization, a2aClient, logger } = require(__commons);
+const cookieParser = require("cookie-parser");
+const storeRouter = require("./routes/store");
+const appLogger = logger.appLogger;
+const errorLogger = logger.errorLogger;
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5006;
 
-app.use(cors());
-app.use(express.json());
-
 const uri = process.env.ATLAS_URI;
 mongoose
 	.connect(uri, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-		createIndexes: true,
+		useCreateIndex: true
 	})
 	.catch(function () {
-		console.log("DB connection error");
+		errorLogger.error("DB connection error");
 	});
 
 const connection = mongoose.connection;
 connection.once("open", () => {
-	console.log(`MongoDB database connection established successfully`);
+	appLogger.info("MongoDB database connection established successfully");
 });
 
-const storeRouter = require("./routes/store");
-
+a2aClient.use(app);
+logger.use(app);
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(authorization.authorizationMiddleware);
+// Attach mongoose connection object as db on each request
+app.use((req, res, next) => {
+	req.db = connection;
+	next();
+});
 app.use("/store", storeRouter);
 
-app.listen(port, () => {
-	console.log(`Server is running on port: ${port}`);
+
+// Error handler
+app.use((err, req, res, next) => {
+	if (err) {
+		res.status(err.status ? err.status : 500);
+		res.json(err.message ? err.message : "Unexpected Error");
+	}
 });
+
+app.listen(port, () => {
+	appLogger.info(`Server is running on port: ${port}`);
+});
+
+process.on("exit", (code) => {
+	connection.close();
+	console.log(`About to exit with code: ${code}`);
+});
+process.on("SIGINT", function () {
+	console.log("Caught interrupt signal");
+	process.exit();
+});
+
+module.exports = {
+	app
+};
